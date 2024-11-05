a82f020ae677119246cd2c3047b8baca8e6ef82e
igorwolfs <igor.wolfs.ur@gmail.com>
2024-11-05 12:25:08 +0100

    [fw] [boards] Added debug configuration

--- boards/arm/bldc/board.cmake
+++ boards/arm/bldc/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32F411VE" "--speed=4000")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw" "--frequency=24000")
 
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
